08a5001df9f256ef380799c6931c96143ad5744d
dhruv <dhruvkathiriya933@gmail.com>
2024-08-04 20:15:05 +0530

    chorme

---
+++ android/app/src/main/kotlin/com/example/googleapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.googleapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/gloabals/list.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> pro1 = [
+  {
+    'image':
+        "https://static.vecteezy.com/system/resources/previews/027/179/391/original/google-sheet-spreadsheet-icon-logo-symbol-free-png.png",
+    'name': " Sheets",
+  },
+  {
+    'image':
+        "https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png ",
+    'name': "Home",
+  },
+  {
+    'image': "https://cdn-icons-png.flaticon.com/512/4438/4438839.png",
+    'name': "Notepad",
+  },
+  {
+    'image':
+        "https://png.pngtree.com/png-vector/20191009/ourmid/pngtree-user-icon-png-image_1796659.jpg",
+    'name': "Login",
+  },
+];
+List<Map<String, dynamic>> pro2 = [
+  {
+    'image':
+        "https://static.vecteezy.com/system/resources/previews/027/179/391/original/google-sheet-spreadsheet-icon-logo-symbol-free-png.png",
+    'name': " Sheets",
+  },
+  {
+    'image':
+        "https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png ",
+    'name': "Home",
+  },
+  {
+    'image': "https://cdn-icons-png.flaticon.com/512/4438/4438839.png",
+    'name': "Notepad",
+  },
+  /* {
+    'icon': const Icon(Icons.add),
+    'name': "Add Shorcut",
+  },*/
+];
